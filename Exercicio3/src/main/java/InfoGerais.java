0186590ab1b50c634f90c9f0767532e4ad707ade
Silmara Lima <silmara.pereiraspl@gmail.com>
2026-09-02 21:05:22 -0300

    Exercício 3 - parcial

---
+++ Exercicio3/src/main/java/InfoGerais.java
@@ -0,0 +1,6 @@
+
+public interface InfoGerais {
+	
+	void exibeInformacoes();
+
+}
---
+++ Exercicio3/src/main/java/Livro.java
@@ -0,0 +1,15 @@
+
+public class Livro {
+	
+	private String autor;
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+
+}
