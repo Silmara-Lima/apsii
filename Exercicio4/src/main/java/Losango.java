4225f51b9ca1a47199f4d241826fb582facefc43
Silmara Lima <silmara.pereiraspl@gmail.com>
2026-09-10 07:23:07 -0300

    Exercício4

--- Exercicio4/src/main/java/Losango.java
+++ Exercicio4/src/main/java/Losango.java
@@ -1,15 +1,31 @@
 public class Losango implements FiguraGeometrica {
 
-    private double diagonalMaior;
-    private double diagonalMenor;
+    private double diagonal1;
+    private double diagonal2;
 
-    public Losango(double diagonalMaior, double diagonalMenor) {
-        this.diagonalMaior = diagonalMaior;
-        this.diagonalMenor = diagonalMenor;
+    public Losango(double diagonal1, double diagonal2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
     }
 
-    public double calculaArea() {
-        return (this.diagonalMaior * this.diagonalMenor) / 2;
+    public double getDiagonal1() {
+		return diagonal1;
+	}
+
+	public void setDiagonal1(double diagonal1) {
+		this.diagonal1 = diagonal1;
+	}
+
+	public double getDiagonal2() {
+		return diagonal2;
+	}
+
+	public void setDiagonal2(double diagonal2) {
+		this.diagonal2 = diagonal2;
+	}
+
+	public double calculaArea() {
+        return (this.diagonal1 * this.diagonal2) / 2;
     }
 
     public String getNomeFigura() {
